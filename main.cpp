b9422f0c56dba0efce4089f76596dd45be5a5523
MohammmadJawad1 <mohammad.jawad30@myhunter.cuny.edu>
2025-04-23 20:53:08 -0400

    main restarted too

--- main.cpp
+++ main.cpp
@@ -3,7 +3,5 @@
 #include "Transform.hpp"
 
 int main() {
-    auto solutions = ChessBoard::findAllQueenPlacements();
-    std::cout << "Total solutions: " << solutions.size() << std::endl;
     return 0;
 }
